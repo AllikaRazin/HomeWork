65c328e44f479523d199737bb190684a5661e916
Allikarazin <AllikaRazin@users.noreply.github.com>
2020-04-02 17:42:37 +0300

    Add HomeWork Iterator

---
+++ Iterator/Iterator/Iterator/ConcreteAggregate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace Iterator
+{ 
+    class ConcreteAggregate : Aggregate
+    {
+        private readonly ArrayList elements = new ArrayList();
+        private ConcreteIterator iterator;
+        public override Iterator CreateIterator()
+        {
+            iterator = new ConcreteIterator(this);
+            return iterator;
+        }
+        public int Count => elements.Count;
+        public override object this[int index] 
+        {
+            get => elements[index];
+            set => elements.Insert(index, value); 
+        }
+    }
+}
---
+++ Iterator/Iterator/Iterator/ConcreteIterator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class ConcreteIterator : Iterator
+    {
+        private readonly ConcreteAggregate aggregate;
+        private int current;
+
+        public ConcreteIterator(ConcreteAggregate aggregate) => this.aggregate = aggregate;
+        public override object First() => aggregate[0];
+        public override object Next()
+        {
+            object element = null;
+
+            if (current < aggregate.Count - 1)
+                element = aggregate[++current];
+
+            return element;
+        }
+        public override object CurrentItem() => aggregate[current];
+        public override bool IsDone() => current >= aggregate.Count - 1;
+    }
+}
